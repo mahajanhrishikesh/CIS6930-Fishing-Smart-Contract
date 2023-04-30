3559d8d88a36c4f9ff1a48b46133b5469d1d36d0
Hrishikesh Mahajan <mahajanhm4@gmail.com>
2023-04-29 21:50:03 -0400

    Create api1Test.sol

---
+++ apiTest/api1Test.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+contract api1Test is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public volume;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    event RequestVolume(bytes32 indexed requestId, uint256 volume);
+
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
+        setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+    }
+
+    function requestVolumeData() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        req.add(
+            "get",
+            "https://civic-genre-325102.ue.r.appspot.com/data/4"
+        );
+
+        req.add("path", "mmsi");
+
+        int256 timesAmount = 1;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    function fulfill(
+        bytes32 _requestId,
+        uint256 _volume
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestVolume(_requestId, _volume);
+        volume = _volume;
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
